b706b737adfd41ee091c767150320a7568164838
Tengyu Wang <tengyu-wang@uiowa.edu>
2014-04-02 21:07:39 -0500

    PropertyHelper

---
+++ DataCollection/Data Collection Server side/src/edu/uiowa/datacollection/util/PropertyHelper.java
@@ -0,0 +1,35 @@
+package edu.uiowa.datacollection.util;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class PropertyHelper {
+
+	private String urladdress;
+	/*
+	 * Other properties can be added here
+	 */
+	public PropertyHelper(String fileName) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(fileName)) ;
+		ArrayList<String> propertyList=new ArrayList<String>();
+		String line;
+		while ((line = br.readLine()) != null) {
+		   propertyList.add(line);
+		}
+		br.close();
+		for(int i=0;i<propertyList.size();i++){
+			String property=propertyList.get(i);
+			String tmp[]=property.split("=");
+			if(tmp[0].equals("URL")){
+				this.urladdress=tmp[1];
+			}
+		}
+		
+	}
+	
+	public String getURLAddress(){
+		return urladdress;
+	}
+}
