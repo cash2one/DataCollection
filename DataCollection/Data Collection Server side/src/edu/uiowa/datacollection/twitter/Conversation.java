4adaf7b69015cd424a444c4613b54d2a3ff5f446
TomWerner <tomwer3@gmail.com>
2014-05-01 16:14:30 -0500

    Tengyu fixed my mistake

--- DataCollection/Data Collection Server side/src/edu/uiowa/datacollection/twitter/Conversation.java
+++ DataCollection/Data Collection Server side/src/edu/uiowa/datacollection/twitter/Conversation.java
@@ -152,22 +152,7 @@ public class Conversation implements Comparable<Conversation>
 
 	public void setStartEndIDCount()
 	{
-		this.startID = messageList.get(0).getmID();
-<<<<<<< HEAD
-		this.endID = messageList.get(messageList.size() - 1).getmID();
-		this.count = messageList.size();
-		String senderID = messageList.get(0).getSender().getTwitterID();
-		String receiverID = messageList.get(0).getRecipients().get(0)
-				.getTwitterID();
-		if (senderID != null && receiverID != null)
-			this.cID = senderID + "_" + receiverID;
-		else
-			this.cID = this.startID + "_" + this.endID;
-	}
-
-	public String getEndID()
-	{
-=======
+		this.startID = messageList.get(0).getmID();		
 		this.endID=messageList.get(messageList.size()-1).getmID();
 		this.count=messageList.size();
 		String senderID=messageList.get(0).getSender().getTwitterID();
@@ -182,7 +167,6 @@ public class Conversation implements Comparable<Conversation>
 	}
 
 	public String getEndID() {
->>>>>>> f31c712d8ca9bf3a28ea037afbfed9123dd63c50
 		return endID;
 	}
 
